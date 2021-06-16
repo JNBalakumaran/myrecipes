ec914f548f6f8aec39cf3a034c3730760b38dd4c
JNBalakumaran <jnbalakumaran@gmail.com>
2021-06-16 16:01:38 +0530

    Edit Chef

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -15,6 +15,24 @@ class ChefsController < ApplicationController
 		end
 	end
 
+	def show
+   	 	@chef = Chef.find(params[:id])
+  	end
+
+  	def edit
+  		@chef = Chef.find(params[:id])
+  	end
+
+  	def update
+  		@chef = Chef.find(params[:id])
+  		if @chef.update(chef_params)
+  			flash[:success] = "Your account was successfully updated"
+  			redirect_to @chef
+  		else
+  			render 'edit'
+  		end
+  	end
+
 	private
 
 		def chef_params
