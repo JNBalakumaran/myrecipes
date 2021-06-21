f65872abaf28291a97719885b68ffe6c87b33a7a
JNBalakumaran <jnbalakumaran@gmail.com>
2021-06-21 18:56:01 +0530

    Ingredients create, update

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
 	post '/login', to: "sessions#create"
 	delete '/logout', to: "sessions#destroy"
 
+	resources :ingredients, except: [:destroy]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
