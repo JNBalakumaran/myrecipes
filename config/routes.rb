378b2e2844411b4c3e61720545f956dacecb9390
JNBalakumaran <jnbalakumaran@gmail.com>
2021-06-22 15:56:16 +0530

    Likes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
 	get 'pages/home', to: 'pages#home'
 
 	resources :recipes do
-		resources :comments, only: [:create]
+  		resources :comments, only: [:create]
+  		member do 
+    		post 'like'
+  		end
 	end
-
 
 	get '/signup', to: 'chefs#new'
 	resources :chefs, except: [:new]
